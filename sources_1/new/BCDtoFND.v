0e823b5d83c9280e3a499ebb2aa3015e6d44c590
ekgh2002 <ekgh2002@naver.com>
2022-10-06 14:50:18 +0900

    second commit

--- sources_1/new/BCDtoFND.v
+++ sources_1/new/BCDtoFND.v
@@ -5,7 +5,7 @@ module BCDtoFND(
     input [3:0] i_value,
     input i_en,
     output [3:0] o_digit,
-    output [7:0] o_fndfont
+    output [7:0] o_fndFont
     );
 
     FND_Select_Decoder fndDigitDecoder(
@@ -17,6 +17,6 @@ module BCDtoFND(
     BCDtoFND_Decoder fndFontDecoder(
     .i_value(i_value),
     .i_en(i_en),
-    .o_font(o_fndfont)
+    .o_font(o_fndFont)
     );
 endmodule
